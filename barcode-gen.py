a24df345c50aac958326ce2f4e6a6cec7b833934
darthnall <blake.nall21@gmail.com>
2023-06-05 15:28:44 -0500

    User input and random number generator

--- barcode-gen.py
+++ barcode-gen.py
@@ -1,5 +1,8 @@
 from barcode import EAN13
 from barcode.writer import SVGWriter
+from random import randint
+import os
+import datetime
 
 '''
     This script generates barcodes and saves them as png files.
@@ -9,11 +12,19 @@ from barcode.writer import SVGWriter
 def main():
 
     # Set the output directory
-    output_dir = 'completed-barcodes'
+    now = datetime.datetime.now()
+    new_dir = now.strftime("%Y-%m-%d-%H-%M-%S")
+    output_dir = f'completed-barcodes/{new_dir}'
+    os.makedirs(output_dir, exist_ok=True)
 
-    # Generate a barcode and save it as an svg file
-    with open(f"{output_dir}/test.svg", "wb") as f:
-        EAN13('111111111111', writer=SVGWriter()).write(f)
+    # Ask user how many barcodes they want to generate
+    total_barcodes = input("How many barcodes do you want to generate? ")
+
+    # Generate total_barcodes barcodes and save them as svg files, starting from 1
+    for i in range(1, int(total_barcodes) + 1):
+        fullname = f"barcode_{i}"
+        with open(f"{output_dir}/{fullname}.svg", "wb") as f:
+            EAN13(str(randint(100000000000, 999999999999)), writer=SVGWriter()).write(f)
 
 if __name__ == '__main__':
     main()
