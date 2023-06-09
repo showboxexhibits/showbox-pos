f1fc3ad801971d18096c6f1c015ae1c278b9c430
darthnall <blake.nall21@gmail.com>
2023-06-09 16:52:00 -0500

    WIP Window testing

--- main.py
+++ main.py
@@ -10,15 +10,37 @@
 
 # These import statements pull from all other associated scripts with this application.
 # It also pulls from the fantastic PyQt5 and  python-barcode libraries.
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+import os
+from PyQt5 import QtGui, QtWidgets
+#from QtCore import Qt
+from QtGui import QPixmap, QBrush, QPalette
+from QtWidgets import QPushButton, QMainWindow, QApplication
 
 # This is the initialization of the main window of the application.
 # This window serves as the tray in which other windows and widgets are placed.
+
+# I have created many syntax errors with the new import statements, fix this demo.
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        image_path = os.path.join(
+            script_dir,
+            "assets/main-screen/main-screen.png"
+        )
+
+        pixmap = QPixmap(image_path)
+        scaled_pixmap = pixmap.scaled(self.width(), self.height(), Qt.KeepAspectRatio)
+
+        palette = QPalette()
+        palette.setBrush(QPalette.Background), QBrush(scaled_pixmap)
+
+        self.setPalette(palette)
         # This is all code for testing purposes, it and this comment will be removed.
+
+        self.setGeometry(0, 0, 1920, 1080)
+
         self.button = QPushButton('Button', self)
         self.button.move(60, 50)
 
