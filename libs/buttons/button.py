8445787d0456de14bc7659352fba7bc2785199e4
darthnall <blake.nall21@gmail.com>
2023-06-14 14:49:39 -0500

    Aligned some buttons

--- libs/buttons/button.py
+++ libs/buttons/button.py
@@ -1,23 +1,30 @@
+import sys
 from PyQt5 import QtWidgets
-
-def clear_all():
-    print("Clicked Clear All")
-
-def delete_last(window):
-    print("Clicked Delete Last")
 
 def make_button(window, name):
     if name == "deleteLast":
         b = QtWidgets.QPushButton(window)
         b.setText("Delete Last")
-        b.move(50, 50)
-        b.clicked.connect(delete_last)
+        b.move(825, 610)
+        b.resize(298, 77)
         return b
     if name == "clearAll":
         b = QtWidgets.QPushButton(window)
         b.setText("Clear All")
-        b.move(50, 20)
-        b.clicked.connect(clear_all)
+        b.move(1153, 609)
+        b.resize(298, 77)
+        return b
+    if name == "close":
+        b = QtWidgets.QPushButton(window)
+        b.setText("Close")
+        b.move(400,400)
+        b.resize(500,500)
+        return b
+    if name == "checkOut":
+        b = QtWidgets.QPushButton(window)
+        b.setText("Checkout")
+        b.move(1445, 789)
+        b.resize(376, 186)
         return b
     else:
         return "error"
--- main.py
+++ main.py
@@ -7,16 +7,27 @@
     end-user interaction with the machine. This GUI has been designed for use with
     a touchscreen interface, rather than a standard keyboard.
 '''
-
-import os
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from libs.buttons.button import make_button
 
+def init_buttons(self):
+    self.deleteLastButton = make_button(self, "deleteLast")
+    self.deleteLastButton.clicked.connect(self.delete_last)
+    self.clearAllButton = make_button(self, "clearAll")
+    self.clearAllButton.clicked.connect(self.clear_all)
+    self.checkOutButton = make_button(self, "checkOut")
+    self.checkOutButton.clicked.connect(self.check_out)
+    self.closeButton = make_button(self, "close")
+    self.closeButton.clicked.connect(self.close_app)         
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        checked = 0
+        
         self.setAnimated(True)
         self.setObjectName('MainWindow')
         stylesheet = """
@@ -29,15 +40,23 @@ class MainWindow(QMainWindow):
 
         self.setStyleSheet(stylesheet)
 
-        make_button(self, "deleteLast")
-        make_button(self, "clearAll")
-        b = QtWidgets.QPushButton(self)
-        b.setText("Close")
-        b.move(200,200)
-        b.clicked.connect(close_app)
+        init_buttons(self)
 
-def close_app():
-    sys.exit()
+    def delete_last(self):
+        print("Deleted last")
+        self.deleteLastButton.setText("Yeet")
+            
+    def clear_all(self):
+        print("Cleared all")
+        self.clearAllButton.setText("Sweet!")
+
+    def check_out(self):
+        print("Checked out")
+        self.checkOutButton.setText("All donezo")
+
+    def close_app(self):
+        sys.exit()
+            
 
 app = QApplication([])
 
